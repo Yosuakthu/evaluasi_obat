c50a98a6163c952d94e8c6c3711978de8a745959
yosuakthu <yosuakatuhupapona@gmail.com>
2022-08-08 10:53:37 +0800

    finis

--- tambahmasuk.php
+++ tambahmasuk.php
@@ -171,7 +171,7 @@
       }
     }else {
       $sql3 = "INSERT INTO masuk 
-      VALUES(NULL,'$satuan','$jum','$jenis','$kategori','$id','$masuk','2','$suplier')";
+      VALUES(NULL,'$satuan','$jum','$jenis','$kategori','$id','$masuk','1','$suplier')";
       $s = mysqli_query($conn,$sql3) or die('gagal 3');
       if ($s) {
         echo "
@@ -198,18 +198,18 @@
 
 
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- DataTables -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<script src="dist/js/demo.js"></script>
 <!-- page script -->
 <script>
   $(function () {
