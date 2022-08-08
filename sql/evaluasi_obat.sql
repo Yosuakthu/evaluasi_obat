5bb4631596d426e93566146e16db76e452d0c91c
yosuakthu <yosuakatuhupapona@gmail.com>
2022-08-08 20:34:19 +0800

    tambah Sql

--- sql/evaluasi_obat.sql
+++ sql/evaluasi_obat.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2022 at 12:18 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Waktu pembuatan: 08 Agu 2022 pada 14.31
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,22 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data_obat`
+-- Struktur dari tabel `data_obat`
 --
 
 CREATE TABLE `data_obat` (
-  `id_data_obat` int(11) NOT NULL,
+  `id_data_obat` varchar(11) NOT NULL,
   `id_pengguna` int(11) NOT NULL,
-  `id_obat` int(11) NOT NULL,
-  `banyak` int(11) NOT NULL,
-  `keterangan_obat` varchar(250) NOT NULL,
+  `id_masuk` int(11) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `keluar` int(11) NOT NULL,
+  `ket_obat` varchar(250) NOT NULL,
   `id_respon` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data_obat`
+--
+
+INSERT INTO `data_obat` (`id_data_obat`, `id_pengguna`, `id_masuk`, `tgl_masuk`, `keluar`, `ket_obat`, `id_respon`) VALUES
+('RST001', 5, 1, '2022-08-06', 2, 'Batuk', 2),
+('RST002', 5, 1, '2022-08-06', 12, 'Batuk\r\n', 2),
+('RST003', 5, 6, '2022-08-06', 12, 'mmm', 2),
+('RST004', 1, 7, '2022-08-06', 1, 'Batuk', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `expire`
+-- Struktur dari tabel `expire`
 --
 
 CREATE TABLE `expire` (
@@ -49,17 +60,20 @@ CREATE TABLE `expire` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `expire`
+-- Dumping data untuk tabel `expire`
 --
 
 INSERT INTO `expire` (`id_expire`, `id_obat`, `expire`) VALUES
-(7, 5, '2022-08-13'),
-(8, 6, '2022-08-06');
+(7, 7, '2022-08-31'),
+(8, 6, '2022-08-06'),
+(13, 5, '2022-08-31'),
+(14, 9, '2022-08-31'),
+(15, 6, '2022-08-31');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jenis`
+-- Struktur dari tabel `jenis`
 --
 
 CREATE TABLE `jenis` (
@@ -68,7 +82,7 @@ CREATE TABLE `jenis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `jenis`
+-- Dumping data untuk tabel `jenis`
 --
 
 INSERT INTO `jenis` (`id_jenis`, `jenis`) VALUES
@@ -79,7 +93,7 @@ INSERT INTO `jenis` (`id_jenis`, `jenis`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori`
+-- Struktur dari tabel `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -88,7 +102,7 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kategori`
+-- Dumping data untuk tabel `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
@@ -100,7 +114,29 @@ INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ket`
+-- Struktur dari tabel `keluar`
+--
+
+CREATE TABLE `keluar` (
+  `id_keluar` int(11) NOT NULL,
+  `id_masuk` int(11) NOT NULL,
+  `keluar` int(11) NOT NULL,
+  `tgl_klr` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `keluar`
+--
+
+INSERT INTO `keluar` (`id_keluar`, `id_masuk`, `keluar`, `tgl_klr`) VALUES
+(2, 1, 2, '2022-08-06'),
+(3, 1, 12, '2022-08-06'),
+(6, 6, 12, '2022-08-06');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ket`
 --
 
 CREATE TABLE `ket` (
@@ -109,7 +145,7 @@ CREATE TABLE `ket` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `ket`
+-- Dumping data untuk tabel `ket`
 --
 
 INSERT INTO `ket` (`id_ket`, `ket`) VALUES
@@ -119,25 +155,35 @@ INSERT INTO `ket` (`id_ket`, `ket`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `masuk`
+-- Struktur dari tabel `masuk`
 --
 
 CREATE TABLE `masuk` (
   `id_masuk` int(11) NOT NULL,
-  `id_obat` int(11) NOT NULL,
   `id_satuan` int(11) NOT NULL,
   `jumblah` int(11) NOT NULL,
   `id_jenis` int(11) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `id_expire` int(11) NOT NULL,
   `masuk` date NOT NULL,
+  `id_ket` int(11) NOT NULL,
   `id_suplier` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `masuk`
+--
+
+INSERT INTO `masuk` (`id_masuk`, `id_satuan`, `jumblah`, `id_jenis`, `id_kategori`, `id_expire`, `masuk`, `id_ket`, `id_suplier`) VALUES
+(1, 3, 60, 2, 1, 7, '2022-08-19', 1, 1),
+(6, 2, 8, 1, 1, 13, '2022-08-28', 1, 1),
+(7, 1, 12, 2, 3, 14, '2022-08-09', 1, 2),
+(8, 3, 2, 3, 3, 15, '2022-08-08', 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `obat`
+-- Struktur dari tabel `obat`
 --
 
 CREATE TABLE `obat` (
@@ -146,18 +192,19 @@ CREATE TABLE `obat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `obat`
+-- Dumping data untuk tabel `obat`
 --
 
 INSERT INTO `obat` (`id_obat`, `obat`) VALUES
 (5, 'Amoxsilin'),
 (6, 'Parasitamol'),
-(7, 'CTM');
+(7, 'CTM'),
+(9, 'Komix');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengguna`
+-- Struktur dari tabel `pengguna`
 --
 
 CREATE TABLE `pengguna` (
@@ -169,29 +216,38 @@ CREATE TABLE `pengguna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pengguna`
+-- Dumping data untuk tabel `pengguna`
 --
 
 INSERT INTO `pengguna` (`id_pengguna`, `nama`, `username`, `pass`, `id_tingkatan`) VALUES
-(1, 'yosua Katuhu, A.Md.Kom', 'yosua', '123', 2),
-(4, 'aa', 'aaa', '123', 3),
+(1, 'Dr.Sumanto, S.Kep., M.Kep', 'Sumanto', '123', 2),
+(4, 'apotek', 'apotek', '123', 3),
 (5, 'Egi Sabanari', 'admin', '123', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `resep`
+-- Struktur dari tabel `resep`
 --
 
 CREATE TABLE `resep` (
   `id_resep` int(11) NOT NULL,
-  `id_data_obat` int(11) NOT NULL
+  `id_data_obat` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `resep`
+--
+
+INSERT INTO `resep` (`id_resep`, `id_data_obat`) VALUES
+(16, 'RST001'),
+(17, 'RST002'),
+(20, 'RST003');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `respon`
+-- Struktur dari tabel `respon`
 --
 
 CREATE TABLE `respon` (
@@ -200,7 +256,7 @@ CREATE TABLE `respon` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `respon`
+-- Dumping data untuk tabel `respon`
 --
 
 INSERT INTO `respon` (`id_respon`, `respon`) VALUES
@@ -211,7 +267,7 @@ INSERT INTO `respon` (`id_respon`, `respon`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `satuan`
+-- Struktur dari tabel `satuan`
 --
 
 CREATE TABLE `satuan` (
@@ -220,7 +276,7 @@ CREATE TABLE `satuan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `satuan`
+-- Dumping data untuk tabel `satuan`
 --
 
 INSERT INTO `satuan` (`id_satuan`, `satuan`) VALUES
@@ -231,7 +287,7 @@ INSERT INTO `satuan` (`id_satuan`, `satuan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `suplier`
+-- Struktur dari tabel `suplier`
 --
 
 CREATE TABLE `suplier` (
@@ -240,7 +296,7 @@ CREATE TABLE `suplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `suplier`
+-- Dumping data untuk tabel `suplier`
 --
 
 INSERT INTO `suplier` (`id_suplier`, `suplier`) VALUES
@@ -250,7 +306,7 @@ INSERT INTO `suplier` (`id_suplier`, `suplier`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tingkatan`
+-- Struktur dari tabel `tingkatan`
 --
 
 CREATE TABLE `tingkatan` (
@@ -259,7 +315,7 @@ CREATE TABLE `tingkatan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tingkatan`
+-- Dumping data untuk tabel `tingkatan`
 --
 
 INSERT INTO `tingkatan` (`id_tingkatan`, `tingkatan`) VALUES
@@ -272,217 +328,224 @@ INSERT INTO `tingkatan` (`id_tingkatan`, `tingkatan`) VALUES
 --
 
 --
--- Indexes for table `data_obat`
+-- Indeks untuk tabel `data_obat`
 --
 ALTER TABLE `data_obat`
   ADD PRIMARY KEY (`id_data_obat`),
   ADD KEY `id_pengguna` (`id_pengguna`),
-  ADD KEY `id_obat` (`id_obat`),
-  ADD KEY `id_respon` (`id_respon`);
+  ADD KEY `id_respon` (`id_respon`),
+  ADD KEY `id_masuk` (`id_masuk`);
 
 --
--- Indexes for table `expire`
+-- Indeks untuk tabel `expire`
 --
 ALTER TABLE `expire`
   ADD PRIMARY KEY (`id_expire`),
   ADD KEY `id_obat` (`id_obat`);
 
 --
--- Indexes for table `jenis`
+-- Indeks untuk tabel `jenis`
 --
 ALTER TABLE `jenis`
   ADD PRIMARY KEY (`id_jenis`);
 
 --
--- Indexes for table `kategori`
+-- Indeks untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indexes for table `ket`
+-- Indeks untuk tabel `keluar`
+--
+ALTER TABLE `keluar`
+  ADD PRIMARY KEY (`id_keluar`);
+
+--
+-- Indeks untuk tabel `ket`
 --
 ALTER TABLE `ket`
   ADD PRIMARY KEY (`id_ket`);
 
 --
--- Indexes for table `masuk`
+-- Indeks untuk tabel `masuk`
 --
 ALTER TABLE `masuk`
   ADD PRIMARY KEY (`id_masuk`),
-  ADD KEY `id_obat` (`id_obat`,`id_satuan`,`id_kategori`,`id_expire`),
+  ADD KEY `id_obat` (`id_satuan`,`id_kategori`,`id_expire`),
   ADD KEY `id_expire` (`id_expire`),
   ADD KEY `id_jenis` (`id_jenis`),
   ADD KEY `id_kategori` (`id_kategori`),
   ADD KEY `id_satuan` (`id_satuan`),
-  ADD KEY `id_suplier` (`id_suplier`);
+  ADD KEY `id_suplier` (`id_suplier`),
+  ADD KEY `id_ket` (`id_ket`);
 
 --
--- Indexes for table `obat`
+-- Indeks untuk tabel `obat`
 --
 ALTER TABLE `obat`
   ADD PRIMARY KEY (`id_obat`);
 
 --
--- Indexes for table `pengguna`
+-- Indeks untuk tabel `pengguna`
 --
 ALTER TABLE `pengguna`
   ADD PRIMARY KEY (`id_pengguna`),
   ADD KEY `id_tingkatan` (`id_tingkatan`);
 
 --
--- Indexes for table `resep`
+-- Indeks untuk tabel `resep`
 --
 ALTER TABLE `resep`
   ADD PRIMARY KEY (`id_resep`),
   ADD KEY `id_data_obat` (`id_data_obat`);
 
 --
--- Indexes for table `respon`
+-- Indeks untuk tabel `respon`
 --
 ALTER TABLE `respon`
   ADD PRIMARY KEY (`id_respon`);
 
 --
--- Indexes for table `satuan`
+-- Indeks untuk tabel `satuan`
 --
 ALTER TABLE `satuan`
   ADD PRIMARY KEY (`id_satuan`);
 
 --
--- Indexes for table `suplier`
+-- Indeks untuk tabel `suplier`
 --
 ALTER TABLE `suplier`
   ADD PRIMARY KEY (`id_suplier`);
 
 --
--- Indexes for table `tingkatan`
+-- Indeks untuk tabel `tingkatan`
 --
 ALTER TABLE `tingkatan`
   ADD PRIMARY KEY (`id_tingkatan`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `data_obat`
---
-ALTER TABLE `data_obat`
-  MODIFY `id_data_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `expire`
+-- AUTO_INCREMENT untuk tabel `expire`
 --
 ALTER TABLE `expire`
-  MODIFY `id_expire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_expire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `jenis`
+-- AUTO_INCREMENT untuk tabel `jenis`
 --
 ALTER TABLE `jenis`
   MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `kategori`
+-- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `ket`
+-- AUTO_INCREMENT untuk tabel `keluar`
+--
+ALTER TABLE `keluar`
+  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `ket`
 --
 ALTER TABLE `ket`
   MODIFY `id_ket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `masuk`
+-- AUTO_INCREMENT untuk tabel `masuk`
 --
 ALTER TABLE `masuk`
-  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `obat`
+-- AUTO_INCREMENT untuk tabel `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `pengguna`
+-- AUTO_INCREMENT untuk tabel `pengguna`
 --
 ALTER TABLE `pengguna`
   MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `resep`
+-- AUTO_INCREMENT untuk tabel `resep`
 --
 ALTER TABLE `resep`
-  MODIFY `id_resep` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_resep` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `respon`
+-- AUTO_INCREMENT untuk tabel `respon`
 --
 ALTER TABLE `respon`
   MODIFY `id_respon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `satuan`
+-- AUTO_INCREMENT untuk tabel `satuan`
 --
 ALTER TABLE `satuan`
   MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `suplier`
+-- AUTO_INCREMENT untuk tabel `suplier`
 --
 ALTER TABLE `suplier`
   MODIFY `id_suplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `tingkatan`
+-- AUTO_INCREMENT untuk tabel `tingkatan`
 --
 ALTER TABLE `tingkatan`
   MODIFY `id_tingkatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `data_obat`
+-- Ketidakleluasaan untuk tabel `data_obat`
 --
 ALTER TABLE `data_obat`
-  ADD CONSTRAINT `data_obat_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `data_obat_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `data_obat_ibfk_3` FOREIGN KEY (`id_respon`) REFERENCES `respon` (`id_respon`) ON DELETE CASCADE;
+  ADD CONSTRAINT `data_obat_ibfk_1` FOREIGN KEY (`id_masuk`) REFERENCES `masuk` (`id_masuk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `data_obat_ibfk_2` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `data_obat_ibfk_3` FOREIGN KEY (`id_respon`) REFERENCES `respon` (`id_respon`);
 
 --
--- Constraints for table `expire`
+-- Ketidakleluasaan untuk tabel `expire`
 --
 ALTER TABLE `expire`
   ADD CONSTRAINT `expire_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `masuk`
+-- Ketidakleluasaan untuk tabel `masuk`
 --
 ALTER TABLE `masuk`
   ADD CONSTRAINT `masuk_ibfk_1` FOREIGN KEY (`id_expire`) REFERENCES `expire` (`id_expire`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `masuk_ibfk_2` FOREIGN KEY (`id_jenis`) REFERENCES `jenis` (`id_jenis`) ON UPDATE CASCADE,
   ADD CONSTRAINT `masuk_ibfk_3` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `masuk_ibfk_4` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `masuk_ibfk_5` FOREIGN KEY (`id_satuan`) REFERENCES `satuan` (`id_satuan`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `masuk_ibfk_6` FOREIGN KEY (`id_suplier`) REFERENCES `suplier` (`id_suplier`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `masuk_ibfk_6` FOREIGN KEY (`id_suplier`) REFERENCES `suplier` (`id_suplier`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `masuk_ibfk_7` FOREIGN KEY (`id_ket`) REFERENCES `ket` (`id_ket`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `pengguna`
+-- Ketidakleluasaan untuk tabel `pengguna`
 --
 ALTER TABLE `pengguna`
   ADD CONSTRAINT `pengguna_ibfk_1` FOREIGN KEY (`id_tingkatan`) REFERENCES `tingkatan` (`id_tingkatan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `resep`
+-- Ketidakleluasaan untuk tabel `resep`
 --
 ALTER TABLE `resep`
-  ADD CONSTRAINT `resep_ibfk_1` FOREIGN KEY (`id_data_obat`) REFERENCES `data_obat` (`id_data_obat`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `resep_ibfk_1` FOREIGN KEY (`id_data_obat`) REFERENCES `data_obat` (`id_data_obat`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
